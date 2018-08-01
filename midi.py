8550070404b6c9f231c6e0b49435c3b6df0018a8
Samuel Brown <sbrow109@vols.utk.edu>
2018-08-01 00:15:20 -0400

    Added licensing information to midi.py.

--- midi.py
+++ midi.py
@@ -1,3 +1,22 @@
+
+#####################################################################
+#                                                                   #
+#  FILENAME -- midi.py                                              #
+#                                                                   #
+#  CREATED  -- July 30, 2018                                        #
+#                                                                   #
+#  AUTHOR   -- Sam Brown                                            #
+#                                                                   #
+#  ACKNOWLEDGEMENTS -- This work draws upon the works of both       #
+#                      Giles F. Hall (which is protected under the  #
+#                      MIT License, 2013), and of colxi (which is   #
+#                      protected under the GNU General Public       #
+#                      License, v3.0).                              #
+#                                                                   #
+#####################################################################
+
+
+import os
 import sys
 import struct
 
@@ -15,136 +34,210 @@ DEFAULT_MIDI_HEADER_SIZE = 14
 # CLASS to store information about a MIDI track
 #
 class Track:
-	def __init__(self):
-		self.events = []
-		print(' Creating track')
-		return
+  def __init__(self):
+    self.events = []
+    print(' Creating track')
+    return
 
 #
 # CLASS to store information about a MIDI event
 #
 class Event:
-	def __init__(self):
-		self.data = None
-		self.delta_time = None
-		self.meta_type = None
-		self.type = None
-		# print('  Creating event')
-		return
+  def __init__(self):
+    self.data = None
+    self.delta_time = None
+    self.meta_type = None
+    self.type = None
+    # print('  Creating event')
+    return
 
 #
+# TODO --> MODIFY
 # CLASS to read a MIDI file into memory
 #
-class MIDI:	
-	
-	def __init__(self):
-		self.tracks = []
-		self.pointer = 0
-		print('Constructing MIDI object...')
-		return
+class MIDI: 
+  
+  def __init__(self):
+    self.tracks = []
+    self.track_size = 0
 
-	#
-	# FUNTION to read a variable length value
-	#
-	def read_vlv(self, track_data):
-		value = 0
-		cont = True # Value continues to next byte
-		while cont:
-			char = track_data[self.pointer]
-			self.pointer += 1
-			if not (char & 0x80):
-				cont = False
-			char = char & 0x7F
-			value = value << 7
-			value += char
-		return value
+    self.file_size = 0
+    self.file_data = None
+    self.pointer = 0
 
-	#
-	# FUNCTION to read a MIDI file
-	#
-	def read_file(self, midi_file):
-		try:
-			with open(midi_file, 'rb') as f:	
-				
-				##### PARSE FILE HEADER CHUNK #####
-				chunk_id = f.read(4)
-				if chunk_id != b'MThd':
-					raise TypeError('\"{}\": File header chunk not found.'.format(midi_file))
-				data = struct.unpack('>LHHH', f.read(10))
-				header_size = data[0]
-				file_format = data[1]
-				num_tracks = data[2]
-				print('Num tracks:', num_tracks)
-				resolution = data[3]
-				if header_size > DEFAULT_MIDI_HEADER_SIZE:
-					f.read(header_size - DEFAULT_MIDI_HEADER_SIZE)
+    print('Constructing MIDI object...')
+    return
 
-				##### PARSE ALL OTHER CHUNKS #####
-				for t in range(num_tracks):
-					chunk_id = f.read(4)
-					if chunk_id == '':
-						break # EOF reached...
-					elif chunk_id != b'MTrk':
-						raise TypeError('\"{}\": Bad track header.'.format(chunk_id))
-					track_size = struct.unpack('>L', f.read(4))[0]
-					print(' track_size:', track_size)
-					track_data = f.read(track_size)
-					pointer = 0
-					# print(track_data[0])
-					# print(track_data[1])
-					self.tracks.append(Track())
+  ########################
+  #
+  # Need to check for EOF every time one of these reading functions is used
+  #
+  ########################
 
-					ec = 0 # Event counter
-					end_of_track = False
-					status_byte = None
-					last_status_byte = None
-					while not end_of_track:
+  # TODO --> Add error checking to this function
+  def read_str(self, num_bytes):
+    start = self.pointer
+    end = self.pointer + num_bytes
+    data = self.file_data[start:end]
+    self.pointer += num_bytes
+    return data.decode('ASCII')
 
-						# Add a new event to the track's event list
-						self.tracks[t].events.append(Event())
-						self.tracks[t].events[ec].delta_time = self.read_vlv(track_data)
-						print(self.tracks[t].events[ec].delta_time)
+  # TODO --> Add error checking to this function
+  def read_int(self, num_bytes):
+    start = self.pointer
+    end = self.pointer + num_bytes
+    data = self.file_data[start:end]
+    self.pointer += num_bytes
+    return int.from_bytes(data, byteorder='big')
 
-						# Get the event's status and compare it to the previous status
-						status_byte = track_data[pointer]
-						self.pointer += 1
-						if status_byte == '':
-							break # EOF Reached
-						elif status_byte >= 128:
-							last_status_byte = status_byte # New status detected
-						else:
-							status_byte = last_status_byte # No new status, reset pointer
-							self.pointer -= 1
+  # TODO --> MODIFY
+  # FUNCTION to read a variable length value
+  def read_vlv(self):
+    value = 0
+    cont = True # Value continues to next byte
+    while cont:
+      # char = track_data[self.pointer]
+      char = self.read_int(1)
+      # self.pointer += 1
+      if not (char & 0x80):
+        cont = False
+      char = char & 0x7F
+      value = value << 7
+      value += char
+    return value
 
-						# META event detected
-						if status_byte == 0xFF:
-							self.tracks[t].events[ec].type = 0xFF
-							meta_type = track_data[self.pointer]
-							self.pointer += 1
-							self.tracks[t].events[ec].meta_type = meta_type
-							meta_length = read_vlv(track_data)
+  #
+  # TODO --> MODIFY
+  # FUNCTION to read a MIDI file
+  #
+  def read_file(self, midi_file):
+    try:
 
-							if 0x2F == meta_type:
-								print('Hello')
+      self.file_size = os.path.getsize(midi_file)
+      print(self.file_size)
+      with open(midi_file, 'rb') as f:
 
-						
+        self.file_data = f.read(self.file_size)
+        chunk_id = self.read_str(4)
+        if chunk_id != 'MThd':
+          raise TypeError('\"{}\": MThd chunk not found.'.format(midi_file))
+        header_size = self.read_int(4)
+        # print(header_size)
+        file_format = self.read_int(2)
+        # print(file_format)
+        num_tracks = self.read_int(2)
+        # print(num_tracks)
+        resolution = self.read_int(2)
+        # print(resolution)
+
+        if header_size > DEFAULT_MIDI_HEADER_SIZE:
+          self.read_int(header_size - DEFAULT_MIDI_HEADER_SIZE) # Read misc bytes
+
+        #
+        # Go through all tracks and add events
+        #
+        for t in range(num_tracks):
+          chunk_id = self.read_str(4)
+          print(chunk_id)
+          if chunk_id != 'MTrk':
+            raise TypeError('\"{}\": MTrk chunk corrupted.'.format(midi_file))
+          self.track_size = self.read_int(4)
+          print(self.track_size)
+          self.tracks.append(Track())
+
+          #
+          # Read events until the end of the current track is reached
+          #
+          ec = 0
+          end_of_track = False
+          status_byte = None
+          last_status_byte = None
+          while not end_of_track:
+
+            # Add a new event to the track's event list
+            self.tracks[t].events.append(Event())
+            self.tracks[t].events[ec].delta_time = self.read_vlv()
+            # print(self.tracks[t].events[ec].delta_time)
+
+            status_byte = self.read_int(1)
+            if status_byte >= 128:
+              last_status_byte = status_byte
+            else:
+              status_byte = last_status_byte
+              self.pointer -= 1
+
+            # print('SB:', status_byte)
 
 
-						ec += 1
+            #
+            # Classify as meta event or other and read data accordingly
+            #
+            if status_byte == 0xFF:
+              self.tracks[t].events[ec].type = 0xFF
+              meta_type = self.read_int(1)
+              meta_length = self.read_vlv()
+              self.tracks[t].events[ec].meta_type = meta_type
 
-						if ec == 100:
-							end_of_track = True
+              # print('. meta_type:', meta_type)
+              # print('. meta_length:', meta_length)
+              if meta_type == 0x2F:
+                end_of_track = True
+              elif (meta_type == 0x01 or meta_type == 0x02 or
+                    meta_type == 0x03 or meta_type == 0x06):
+                self.tracks[t].events[ec].data = self.read_str(meta_length)
+              elif (meta_type == 0x21 or meta_type == 0x59 or
+                    meta_type == 0x51):
+                self.tracks[t].events[ec].data = self.read_int(meta_length)
+              elif meta_type == 0x54:
+                self.tracks[t].events[ec].data = []
+                for i in range(0, 5):
+                  self.tracks[t].events[ec].data.append(self.read_int(1))
+              elif meta_type == 0x58:
+                self.tracks[t].events[ec].data = []
+                for i in range(0, 4):
+                  self.tracks[t].events[ec].data.append(self.read_int(1))
+              else:
+                print('DEFAULT META CASE HANDLED.')
 
-					print('Events:', ec)
+            #
+            # NOT a meta event, treat as sysex or other event type
+            #
+            else:
+              event_type = status_byte >> 4
+              channel = status_byte & 0x0F
+              # print('ET', event_type)
+              # print('Ch', channel)
+              self.tracks[t].events[ec].type = event_type
+              self.tracks[t].events[ec].channel = channel ### Not defined but it still works?
 
+              if event_type == 0xF:
+                print('Sysex event.')
+              elif (event_type == 0xA or event_type == 0xB or
+                    event_type == 0xE or event_type == 0x8 or
+                    event_type == 0x9):
+                self.tracks[t].events[ec].data = []
+                for i in range(0, 2):
+                  self.tracks[t].events[ec].data.append(self.read_int(1))
+              elif (event_type == 0xC or event_type == 0xD):
+                self.tracks[t].events[ec].data = self.read_int(1)
+              else:
+                print('DEFAULT REG CASE HANDLED')
 
-		except TypeError as err:
-			print(err)
+            # Increment the number of events encountered thus far
+            ec += 1
 
-		return
+        
+          print('Finished reading track.')
+          # print(self.tracks[t].events)
+
+        
+    except TypeError as err:
+      print(err)
+
+    return
 
 if __name__ == '__main__':
-	filename = sys.argv[1]
-	midi = MIDI()
-	midi.read_file(filename)
-	print('FINISHED')
+  filename = sys.argv[1]
+  midi = MIDI()
+  midi.read_file(filename)
+  print('FINISHED')
